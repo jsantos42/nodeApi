e5d99461994e4b492bf90a3cfe2ab9b7430cdbd9
João Almeida Santos <79864209+jsantos42@users.noreply.github.com>
2023-09-30 04:18:03 +0200

    fix: remove rendering inexistent template

--- src/app.ts
+++ src/app.ts
@@ -52,9 +52,7 @@ app.use(function(req: Request, res: Response, next: NextFunction) {
 app.use(function(err: HttpError, req: Request, res: Response, next: NextFunction) {
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
-
 	res.status(err.status || 500);
-	res.render('error');
 })
 
 //==============================================================================
